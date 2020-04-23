6eaa6816888b3629ea2dcd5464fbb2a746c21982
wes <jwesheath@gmail.com>
2020-04-23 16:07:53 -0400

    Updates

---
+++ precache-manifest.3171800eb56ffa063cc197aa66be56e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b5af38b6d01f485184a6f5eff81a89",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "827b154f2674c3b80042",
+    "url": "/covid-tracker/static/js/2.a819d754.chunk.js"
+  },
+  {
+    "revision": "6d31b1644b38d9ac876f9e05cdffaf03",
+    "url": "/covid-tracker/static/js/2.a819d754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450a04d1ccb41765ebe4",
+    "url": "/covid-tracker/static/js/main.10e8509a.chunk.js"
+  },
+  {
+    "revision": "b322d784313ff59d4b8f",
+    "url": "/covid-tracker/static/js/runtime-main.52767e05.js"
+  }
+]);
